019bc161eaf4a8391ef95e74d8139972f7ce85c8
Damse96 <eaaankd@students.eaaa.dk>
2023-08-24 10:00:03 +0200

    yes

--- L01_GIT_opsaetning/src/Main.java
+++ L01_GIT_opsaetning/src/Main.java
@@ -1,16 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-
         System.out.println("Hello world!");
-
-        System.out.println("behold i am become death, destroyer of worlds");
-
-        System.out.println("to Democracy anakin");
-
-        System.out.println("i have found peace");
-
-        System.out.println("yeas");
-
-        System.out.println();
     }
 }
