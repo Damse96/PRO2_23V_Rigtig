d177ed1fb23e93371ba107804da34aed83ffafc0
Damse96 <eaaankd@students.eaaa.dk>
2023-08-24 09:23:50 +0200

    yes

--- L01_GIT_opsaetning/src/Main.java
+++ L01_GIT_opsaetning/src/Main.java
@@ -3,6 +3,6 @@ public class Main {
 
         System.out.println("Hello world!");
 
-        System.out.println("i have become death, destroyer of worlds");
+        System.out.println("behold i am become death, destroyer of worlds");
     }
 }
