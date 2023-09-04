5b4df88e27c3cb5dc58a24411d4d270a12d2ac22
Damse96 <eaaankd@students.eaaa.dk>
2023-09-04 09:24:43 +0200

    yes

--- L02_Specialisering_nedarvning_og_polymorfi_del_1/src/opgave3/App.java
+++ L02_Specialisering_nedarvning_og_polymorfi_del_1/src/opgave3/App.java
@@ -1,4 +1,19 @@
 package opgave3;
 
+import Opgave1.Main;
+
+import java.time.LocalDate;
+
 public class App {
+    public static void main(String[] args) {
+
+        Product product1 = new Product(2,"Anders", 200);
+        Product product2 = new Product(1,"mads",10);
+        Product product3 = new Product(3,"Minik", 50);
+        Product product4 = new Product(4,"Thor",300);
+        Product product5 = new Product(5,"Majken", 15);
+        Customer customer1 = new Customer("Madser", LocalDate.of(2020,9,21));
+        Customer customer2 = new Customer("Anne-Sophie", LocalDate.of(2000,8,15));
+
+    }
 }
---
+++ L03_Abstrakte_Indkapsling/src/Mekanikervërksted/ArbejdsDreng.java
@@ -0,0 +1,14 @@
+package Mekanikervërksted;
+
+public class ArbejdsDreng extends Ansat{
+
+
+    public ArbejdsDreng(String navn, String adresse, double timeløn) {
+        super(navn, adresse, timeløn);
+    }
+
+    @Override
+    public double beregnLøn() {
+        return getTimeløn() * 25;
+    }
+}
